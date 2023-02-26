7a3dd2b001588e6fe2a04f1435eae5ec37184674
PrashantHanda011 <handaprashant719@gmail.com>
2023-02-27 00:04:37 +0530

    new upodates

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='d-flex justify-content-betweeen'>
+            <button></button>
+        </div>
+    );
+}
+
+export default Footer;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../Styles/home.css'
-function Home() {
+import captain from '../Assets/frontpage/captain.jpg'
+import doctor from '../Assets/frontpage/doctor.jfif'
+import scarlet from '../Assets/frontpage/scarlet.jfif'
+import thor from '../Assets/frontpage/thor.jfif'
+import iron from '../Assets/frontpage/iron.jfif'
+const Home = () => {
+    const [character, setcharacter] = useState(0);
     return (
-        <>
-            <div className='col-12 d-flex'>
-                <div className='col-2  side-design'>
+        <div className='container-fluid p-0 home_box  p-2'>
+            <div className='character_container my-2'>
+                <h2 className='my-5'>Choose Your Character</h2>
+                <ul className='d-flex container justify-content-center align-items-center'>
+                    <li>
+                        <img onClick={() => setcharacter(1)} className={`${character === 1 ? "character_selected_img" : null}`} src={captain} alt="captain" />
+                        <h5 className={`${character === 1 ? "character_selected_text" : null}`}>Captain America</h5>
+                    </li>
+                    <li>
+                        <img onClick={() => setcharacter(2)} className={`${character === 2 ? "character_selected_img" : null}`} src={doctor} alt="doctor strange" />
+                        <h5 className={`${character === 2 ? "character_selected_text" : null}`}>Docter Strange</h5>
 
-                </div>
-                  <div className='d-flex flex-column col-8 ms-5 justify-content-center align-items-center'>
-                        <div className='col-8'>
-                                <h3>Q1 . Why Am I Doing This ?</h3>
-                                <p className='mt-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus nisi, in omnis minima possimus repellat, explicabo consectetur dignissimos ipsa adipisci quas rem quisquam doloribus. Velit a saepe debitis omnis? Vitae.</p>
-                        </div>
+                    </li>
+                    <li>
+                        <img onClick={() => setcharacter(3)} className={`${character === 3 ? "character_selected_img" : null}`} src={scarlet} alt="scarlet" />
+                        <h5 className={`${character === 3 ? "character_selected_text" : null}`}>Scarlet Witch</h5>
+                    </li>
+                    <li>
+                        <img onClick={() => setcharacter(4)} className={`${character === 4 ? "character_selected_img" : null}`} src={thor} alt="thor" />
+                        <h5 className={`${character === 4 ? "character_selected_text" : null}`}>Thor</h5>
+                    </li>
+                    <li>
+                        <img onClick={() => setcharacter(5)} className={`${character === 5 ? "character_selected_img" : null}`} src={iron} alt="iron man" />
+                        <h5 className={`${character === 5 ? "character_selected_text" : null}`}>Iron Man</h5>
 
-                        <div className='mt-4 d-flex justify-content-end w-100'>
-                            <button className='navigate-btn'>Next</button>
-                        </div>
-                  </div>  
-
+                    </li>
+                </ul>
             </div>
-        </>
-    )
+            <div className='home_footer d-flex justify-content-end container'>
+                {
+                    character >= 1 && character <= 5 &&
+                    <button className='btn btn-danger'>Next</button>
+                }
+            </div>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
