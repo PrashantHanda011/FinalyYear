7f91d0781ce1227eb0f6a4675f9fa6b7a835bde7
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2023-03-05 18:40:00 +0530

    landing page

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../Styles/LandingPage.css'
+const styles = {
+ 
+};
+
+const LandingPage = () => {
+  return (
+    <div > 
+      <div className='centerContainer'>
+        <p className='text'>What's ur Personality</p>
+      </div>
+      <div className='bottomContainer'>This container is at the bottom of the page</div>
+    </div>
+  );
+};
+
+export default LandingPage
