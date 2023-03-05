792bfb5de7057bde99480b0df75ea1ae0dee0a39
Warisha32 <66689362+Warisha32@users.noreply.github.com>
2023-03-05 19:57:55 +0530

    Update LandingPage.jsx

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../Styles/LandingPage.css'
+import backgroundImage from '../Assets/frontpage/bcg.webp';
 const styles = {
  
 };
@@ -8,8 +9,13 @@ const styles = {
 const LandingPage = () => {
   return (
     <div > 
-      <div className='centerContainer'>
-        <p className='text'>What's ur Personality</p>
+      <div  className='centerContainer' style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
+    }}>
+        <p className='text'>What's ur Personality?</p>
       </div>
       <div className='bottomContainer'>This container is at the bottom of the page</div>
     </div>
